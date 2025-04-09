281ba764e19905f58aadcc73bb7f0238747bff0d
James Boot <bootj@babs002.nemo.thecrick.org>
2025-04-09 10:54:10 +0100

    Add fastqc script for post processing.

---
+++ tRNA/03_fastqc.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=fastqc
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=ncpu
+#SBATCH --time='72:00:00'
+#SBATCH --mem=32G
+
+# Modules
+ml purge
+ml FastQC/0.11.8-Java-1.8
+ml MultiQC/1.25.1
+
+# Define directories
+PROJDIR=/nemo/stp/babs/working/bootj/projects/bauerd/nuno.santos/trna_shape
+RESULTSDIR=${PROJDIR}/01_preprocess_reads_outs
+INPUTDIR=${RESULTSDIR}/07_adjusted_header
+FASTQCDIR=${RESULTSDIR}/08_fastqc
+
+# Make directories
+mkdir -p ${FASTQCDIR}
+
+# FastQC loop
+for i in ${INPUTDIR}/*.fq.gz
+do
+        fastqc -o ${FASTQCDIR} $i
+done
+
+# MultiQC
+multiqc ${FASTQCDIR} --outdir ${FASTQCDIR} --filename multiqc_report.html
